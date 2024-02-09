6639e8734cc2f72338f012508daa2437c96e1323
Kurt Bruns <12160501+kurtbruns@users.noreply.github.com>
2024-02-09 15:21:15 -0800

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 
+import { Scene } from '@kurtbruns/vector';
 import './initialize';
 
 // Insert your code here
-import './ComplexNumbers';
+class ExampleScene extends Scene {
+    constructor(){
+        super();
+
+        this.frame.control(100,100);
+    }
+}
+
+new ExampleScene();
